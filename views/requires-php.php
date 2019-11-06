16d6b09cba4f8b4048620f137b67662410ae6fb0
Ross McKay <rmckay@webaware.com.au>
2019-11-06 14:10:36 +1100

    update recommended PHP version and php.net link

--- views/requires-php.php
+++ views/requires-php.php
@@ -9,8 +9,8 @@ if (!defined('ABSPATH')) {
 		<?php echo gf_heidelpay_external_link(
 				sprintf(esc_html__('Gravity Forms heidelpay requires PHP %1$s or higher; your website has PHP %2$s which is {{a}}old, obsolete, and unsupported{{/a}}.', 'gf-heidelpay'),
 					esc_html(GFHEIDELPAY_PLUGIN_MIN_PHP), esc_html(PHP_VERSION)),
-				'https://secure.php.net/supported-versions.php'
+				'https://www.php.net/supported-versions.php'
 			); ?>
 	</p>
-	<p><?php printf(esc_html__('Please upgrade your website hosting. At least PHP %s is recommended.', 'gf-heidelpay'), '7.2'); ?></p>
+	<p><?php printf(esc_html__('Please upgrade your website hosting. At least PHP %s is recommended.', 'gf-heidelpay'), '7.3'); ?></p>
 </div>
